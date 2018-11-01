29257eb668bbc9f86f58890ecbddadc8588ae3e5
oleksandr <oleksandr@192.168.0.84>
2018-11-01 22:04:35 +0200

    версия для wimex

--- SitemapXML.php
+++ SitemapXML.php
@@ -59,7 +59,7 @@ class SitemapXML implements SitemapXMLInterface {
     private function header () {
         $this->code .= '<?xml version="1.0" encoding="UTF-8"?>
                             <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-                            <!-- Last update of sitemap 2018-05-10 17:15:58+02:00 -->';
+                            <!-- Last update of sitemap ' . date('c') . ' -->';
     }
     
     private function footer () {
